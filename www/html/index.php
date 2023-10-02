b4ae156b6d5d68e6db4af3b437dd9ff10558c1af
finding-hiko <ok919872i@gmail.com>
2023-10-02 21:54:06 +0900

    お問合わせボックスの枠追加

--- www/html/index.php
+++ www/html/index.php
@@ -8,8 +8,15 @@
 </head>
 
 <body>
+  <form action="./index.php" method="post">
+    名前<input type="text" name="full-name" value=""><br>
+    Eメール<input type="email" name="email" value=""><br>
+    お問い合わせ内容<br>
+    <textarea name="message" cols="40" rows="8"></textarea><br>
+    <input type="submit" name=" button" value="確認">
 
 
+  </form>
 
 </body>
 

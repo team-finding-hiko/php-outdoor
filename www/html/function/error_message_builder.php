0531e0dc885e14c28e4e32be90553dbca1ddb0fc
finding-hiko <ok919872i@gmail.com>
2023-10-22 15:42:59 +0900

    バージョン変更

--- www/html/function/error_message_builder.php
+++ www/html/function/error_message_builder.php
@@ -25,11 +25,12 @@
 // }
 
 // 名前を入れた時にエラーメッセージを返却するクラス
+
 class NameErrorMessageBuilder
 {
 
   // クラスの状態（車でいう速度）
-  private $form_field_name;
+  private string $form_field_name;
 
   function __construct()
   {
@@ -37,19 +38,19 @@ class NameErrorMessageBuilder
   }
 
   // クラスの状態に関するメソッド（確認：車でいうメーター）
-  public function getFormFieldName()
+  public function getFormFieldName(string $value): string
   {
     return $this->form_field_name;
   }
 
-  public function getErrorMessage()
+  public function getErrorMessage(): ?string
   {
     if (!$_POST[$this->form_field_name]) {
       return "名前を入力してください";
     } else if (mb_strlen($_POST[$this->form_field_name]) > 100) {
       return "名前は100文字以内にしてください";
     }
-    return "";
+    return null;
   }
 }
 
@@ -80,7 +81,8 @@ class MailErrorMessageBuilder
     } else if (!filter_var($_POST[$this->form_field_name], FILTER_VALIDATE_EMAIL)) {
       return "メールアドレスが不正です";
     }
-    return "";
+    return null;
   }
 }
+
 ?>

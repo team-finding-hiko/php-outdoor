a25ae644e2c5b4cb1e251577da920a6676cf0ac3
finding-hiko <ok919872i@gmail.com>
2023-11-15 00:12:40 +0900

    cssを別ファイルへ

--- html/index.php
+++ html/index.php
@@ -95,27 +95,7 @@ if (isset($_POST[BACK]) && $_POST[BACK]) {
   <title>お問い合わせフォーム</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <style>
-    body {
-      padding: 10px;
-      max-width: 600px;
-      margin: 0px auto;
-
-    }
-
-    tbody,
-    td,
-    tfoot,
-    th,
-    thead,
-    tr {
-      border-width: 1px;
-    }
-
-    div.button {
-      text-align: center;
-    }
-  </style>
+  <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>

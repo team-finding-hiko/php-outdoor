fd045595360e97c98ca12324c17a28becd382f0b
finding-hiko <ok919872i@gmail.com>
2023-11-15 21:58:57 +0900

    背景に雨表現のCSSを追加

--- html/index.php
+++ html/index.php
@@ -95,7 +95,8 @@ if (isset($_POST[BACK]) && $_POST[BACK]) {
   <title>お問い合わせフォーム</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="./style/style.css">
+  <link rel="stylesheet" href="./style/rain.css">
 </head>
 
 <body>
@@ -109,6 +110,20 @@ if (isset($_POST[BACK]) && $_POST[BACK]) {
     }
     ?>
     <?php include './common/weather.php'; ?>
+
+    <div class="rains">
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+    </div>
+    <div class="splat">
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+    </div>
 
     <form action="./index.php" method="post">
 
